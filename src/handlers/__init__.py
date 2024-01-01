b138eba3dc9927faea76928d204ba2cd76c171aa
Saveliy <s.lekhtin@innopolis.university>
2024-01-01 15:49:26 +0500

    chore: add draft for My Bookings inline buttons

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,6 +1,6 @@
 from src.handlers.create_booking import router as router_create_bookings
 from src.handlers.registration import router as router_registration
 from src.handlers.schedule import router as router_image_schedule
-from src.handlers.bookings import router as router_bookings
+from src.handlers.my_bookings import router as router_bookings
 
 routers = [router_create_bookings, router_registration, router_image_schedule, router_bookings]
--- src/handlers/my_bookings.py
+++ src/handlers/my_bookings.py
@@ -1,8 +1,9 @@
 import json
+from datetime import datetime
 
 import aiohttp
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from src.handlers.registration import is_user_exists
 from src.keyboards import registration
@@ -20,6 +21,19 @@ async def get_participant_id(telegram_id: int):
             return response_json
 
 
+async def create_inline_keyboard(n: int, bookings: list[dict]):
+    keyboard = [[]]
+
+    for i in range(n):
+        button1 = InlineKeyboardButton(text=f"{bookings[i]['time_start']} - {bookings[i]['time_end'][-5:]}",
+                                       callback_data=f'action_{2 * i + 1}')
+        button2 = InlineKeyboardButton(text='Удалить', callback_data=f'delete_booking_{i}')
+
+        keyboard += [[button1, button2]]
+
+    return InlineKeyboardMarkup(inline_keyboard=keyboard, row_width=2)
+
+
 @router.message(F.text == "My bookings")
 async def show_my_bookings(message: Message):
     if not await is_user_exists(str(message.from_user.id)):
@@ -34,11 +48,16 @@ async def show_my_bookings(message: Message):
             async with session.get(url, params=params) as response:
                 response_text = await response.text()
                 response_json = json.loads(response_text)
-                print(response_json)
 
-        msg = ''
+        if not len(response_json):
+            await message.answer("You don`t have active bookings.")
+        else:
+            bookings = [get_pretty_datetime(entry) for entry in response_json]
+            await message.answer("Inline buttons:", reply_markup=await create_inline_keyboard(len(bookings), bookings))
 
-        for booking in response_json:
-            msg += booking['time_start'] + " " + booking['time_end'] + "\n"
 
-        await message.answer(msg)
+def get_pretty_datetime(data):
+    for key in ['time_start', 'time_end']:
+        date_object = datetime.strptime(data[key], "%Y-%m-%dT%H:%M:%S")
+        data[key] = date_object.strftime("%B %d %H:%M")
+    return data
