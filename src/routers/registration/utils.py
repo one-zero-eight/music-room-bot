6a414fc22995a646c57a19956db309dd449f43a9
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2024-01-14 14:15:49 +0300

    chore: ruff formatting

--- src/routers/registration/utils.py
+++ src/routers/registration/utils.py
@@ -1,7 +1,6 @@
 async def is_cyrillic(string: str):
     alpha = "йцукенгшщзхъфывапролджэячсмитьбю`ё"
     # Remove spaces from the string
-    string_without_spaces = ''.join(char for char in string if char != ' ').lower()
+    string_without_spaces = "".join(char for char in string if char != " ").lower()
 
     return all(char in alpha for char in string_without_spaces)
-
